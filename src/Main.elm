82b6c983e8e7b574e7fa377e8565dccd4bdf044a
Jeremy H. Brown <jhbrown@gmail.com>
2019-03-18 13:20:19 -0400

    Update a few copyright headers.

--- src/Main.elm
+++ src/Main.elm
@@ -3,7 +3,6 @@
 -- The copyright holder licenses this file to you under the MIT License (the
 -- "License"); you may not use this file except in compliance with the License.
 -- You may obtain a copy of the License at https://opensource.org/licenses/MIT
--- Copyright 2019 Jeremy H. Brown
 
 
 module Main exposing (main)
--- src/PageMsg.elm
+++ src/PageMsg.elm
@@ -1,3 +1,10 @@
+-- Copyright 2019 Jeremy H. Brown
+--
+-- The copyright holder licenses this file to you under the MIT License (the
+-- "License"); you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at https://opensource.org/licenses/MIT
+
+
 module PageMsg exposing (PageMsg(..))
 
 import Page.About as About

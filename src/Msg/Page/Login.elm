bfa0b787a84cbaac8aadfd8d115f50229af21cbf
Jeremy H. Brown <jhbrown@gmail.com>
2019-03-18 13:18:54 -0400

    We don't need this file anymore.

--- src/Msg/Page/Login.elm
+++
@@ -1,8 +0,0 @@
--- Copyright 2019 Jeremy H. Brown
---
--- The copyright holder licenses this file to you under the MIT License (the
--- "License"); you may not use this file except in compliance with the License.
--- You may obtain a copy of the License at https://opensource.org/licenses/MIT
-
-
-module Msg.Page.Login exposing (Msg(..))
